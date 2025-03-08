8f0e374d561b5143ac653a50cdbc87ed0d3a37f9
Gabrielle Desiree <gabrielle.desiree@gmail.com>
2025-03-08 09:43:16 -0300

    criação do demo service e remoção do construtor padrão

---
+++ app/src/main/aidl/com/grupo10/aidl_demo/IMyAidlInterface.aidl
@@ -0,0 +1,11 @@
+// IMyAidlInterface.aidl
+package com.grupo10.aidl_demo;
+
+// Declare any non-default types here with import statements
+
+interface IMyAidlInterface {
+
+     int somar(int a, int b);
+     int mudar_cor(int color);
+     void mandar_mensagem(String mensagem);
+}
---
+++ app/src/main/java/com/grupo10/aidl_demo/DemoService.java
@@ -0,0 +1,16 @@
+package com.grupo10.aidl_demo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class DemoService extends Service {
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
