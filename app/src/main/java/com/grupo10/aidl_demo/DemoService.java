7250cad7351931ca0e30bef05adec1a5d667ac70
Gabrielle Desiree <gabrielle.desiree@gmail.com>
2025-03-08 09:53:06 -0300

    Adicionando uma instancia do Binder + inicio da implementação dos métodos

--- app/src/main/java/com/grupo10/aidl_demo/DemoService.java
+++ app/src/main/java/com/grupo10/aidl_demo/DemoService.java
@@ -3,9 +3,27 @@ package com.grupo10.aidl_demo;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
 
 public class DemoService extends Service {
 
+
+    private IMyAidlInterface.Stub binder = new IMyAidlInterface.Stub() {
+        @Override
+        public int somar(int a, int b) throws RemoteException {
+            return 0;
+        }
+
+        @Override
+        public int mudar_cor(int color) throws RemoteException {
+            return 0;
+        }
+
+        @Override
+        public void mandar_mensagem(String mensagem) throws RemoteException {
+
+        }
+    };
 
 
     @Override
