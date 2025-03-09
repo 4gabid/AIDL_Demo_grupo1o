6c8a6fc97049b0d2a2ca84350d7f64e3bf7c4a95
Gabrielle Desiree <gabrielle.desiree@gmail.com>
2025-03-09 10:50:04 -0300

    conexão e uso dos métodos na Main Activitiy

--- app/src/main/java/com/grupo10/aidl_demo/DemoService.java
+++ app/src/main/java/com/grupo10/aidl_demo/DemoService.java
@@ -2,9 +2,12 @@ package com.grupo10.aidl_demo;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
+
+import java.util.Random;
 
 public class DemoService extends Service {
 
@@ -12,18 +15,25 @@ public class DemoService extends Service {
     private IMyAidlInterface.Stub binder = new IMyAidlInterface.Stub() {
         @Override
         public int somar(int a, int b) throws RemoteException {
-            return 0;
+            return a+b;
         }
 
         @Override
         public int mudar_cor(int color) throws RemoteException {
-            return 0;
+
+            Random random = new Random(color);
+
+            int red = random.nextInt(256);
+            int green = random.nextInt(256);
+            int blue = random.nextInt(256);
+
+            return Color.rgb(red, green, blue);
         }
 
         @Override
         public void mandar_mensagem(String mensagem) throws RemoteException {
 
-            Log.d(  "Demo Service", "Message sent: "+ mensagem);
+            Log.d(  "Demo_Service", "Resultado:"+ mensagem);
         }
     };
 
---
+++ app/src/main/java/com/grupo10/aidl_demo/MainActivity.java
@@ -0,0 +1,102 @@
+package com.grupo10.aidl_demo;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private IMyAidlInterface demoService;
+    private boolean isBound = false;
+
+
+    private EditText num1, num2;
+    private TextView resultado;
+    private Button btnCalcular;
+
+
+    private ServiceConnection serverConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            demoService = IMyAidlInterface.Stub.asInterface(iBinder);
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isBound =false;
+            demoService = null;
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.grupo10.aidl_demo", "com.grupo10.aidl_demo.DemoService"));
+
+        bindService(intent, serverConnection, BIND_AUTO_CREATE);
+
+
+        btnCalcular= findViewById(R.id.btnCalcular);
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        resultado = findViewById(R.id.resultado);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int valor1 = Integer.parseInt(num1.getText().toString());
+                    int valor2 = Integer.parseInt(num2.getText().toString());
+                    int resultadoSoma = demoService.somar(valor1, valor2);
+                    resultado.setText("Resultado: " + resultadoSoma);
+                    demoService.mandar_mensagem(" "+ resultadoSoma);
+                    int novaCor = demoService.mudar_cor(resultadoSoma);
+                    btnCalcular.setBackgroundColor(novaCor);
+
+                }catch (RemoteException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(isBound){
+          isBound = false;
+          unbindService(serverConnection);
+
+        }
+    }
+}
